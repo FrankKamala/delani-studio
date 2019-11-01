4995c47e0c94c970b72f04eac3111fd599286bab
FrankKamala <frankkamala@gmail.com>
2019-11-01 16:48:38 +0300

    added script functions for hide and show classes

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".design").click(function (event) {
+      $(".hide").slideToggle();
+      $(".show").slideToggle();
+
+
+  });
+  /*display and hive development content*/
+  $(".development").click(function (event) {
+    $(".show1").slideToggle();
+    $(".hide1").slideToggle();
+  });
+
+  $(".product").click(function (event) {
+    $(".show2").slideToggle();
+    $(".hide2").slideToggle();
+  });
+
+});
