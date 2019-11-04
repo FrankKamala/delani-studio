dc4829a0769d12559ecf676487e49e6b5fc55f36
FrankKamala <frankkamala@gmail.com>
2019-11-04 11:30:33 +0300

    fix indentation on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-$(document).ready(function () {
-  $(".design").click(function (event) {
-      $(".hide").slideToggle();
-      $(".show").slideToggle();
+$(document).ready(function() {
+  $(".design").click(function(event) {
+    $(".hide").slideToggle();
+    $(".show").slideToggle();
 
 
   });
   /*display and hive development content*/
-  $(".development").click(function (event) {
+  $(".development").click(function(event) {
     $(".show1").slideToggle();
     $(".hide1").slideToggle();
   });
 
-  $(".product").click(function (event) {
+  $(".product").click(function(event) {
     $(".show2").slideToggle();
     $(".hide2").slideToggle();
   });
-  $("button").click(function(event){
+  $("button").click(function(event) {
     alert("Message Sent");
     event.preventDefault();
 
